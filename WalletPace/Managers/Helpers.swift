d1aa1864152d591afe6a97084705fe9dadfffc86
Pedro Silva <pedro.silva@salesrabbit.com>
2024-04-16 20:16:29 +0100

    hiding logic

--- WalletPace/Managers/Helpers.swift
+++ WalletPace/Managers/Helpers.swift
@@ -2,7 +2,32 @@
 //  Helpers.swift
 //  WalletPace
 //
-//  Created by Pedro Silva 🐰 on 16/04/2024.
+//  Created by Pedro Silva on 16/04/2024.
 //
 
 import Foundation
+
+//        var liabilities: [Liability] = []
+//        var incomes: [Income] = []
+        
+//        var amount: Double { wallet?.amount ?? 0 }
+        
+//        var walletInAWeek: Double { amount + (incrementPace*60*60*24*7) }
+//        var walletInAMonth: Double { amount + (incrementPace*60*60*24*30) }
+//        var walletInAYear: Double { amount + (incrementPace*60*60*24*365) }
+//        var walletOnYearEnd: Double { 0 }
+//
+//        var incrementInASecond: Double { incrementPace }
+//        var incrementInAMinute: Double { incrementPace*60 }
+//        var incrementInAHour: Double { incrementPace*60*60 }
+//        var incrementInADay: Double { incrementPace*60*60*24 }
+//        var incrementInAMonth: Double { incrementPace*60*60*24*30 }
+//        var incrementInAYear: Double { incrementPace*60*60*24*30*12 }
+        
+//        var incrementPace: Double {
+//            let sumIncome = incomes.reduce(0, { return $0 + $1.amount })
+//            let sumLiability = liabilities.reduce(0, { return $0 + $1.amount })
+//            // its being measured by month
+//            return ((sumIncome*12/365/24/60/60) - (sumLiability*12/365/24/60/60))
+//        }
+        
